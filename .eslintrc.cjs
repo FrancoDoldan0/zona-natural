35d3ecf372017093bbf930e131a0b7884387a909
franco <francodoldan23@gmail.com>
2025-09-02 21:49:46 -0300

    chore: drop deprecated next-on-pages rule; use recommended preset

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals'],
+  extends: ['plugin:next-on-pages/recommended', 'next/core-web-vitals'],
   plugins: ['next-on-pages'],
   rules: {
-    'next-on-pages/no-top-level-getrequestcontext': 'error',
   },
 };
+
