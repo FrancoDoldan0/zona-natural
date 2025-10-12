7b1d0746f835d14df80830a63c897532f300bf11
franco <francododlan23@gmail.com>
2025-10-12 14:17:18 -0300

    fix(layout): agrego cart provider

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 // app/(public)/layout.tsx
 import "../globals.css";
+import CartProvider from "@/components/cart/CartProvider";
 
 export default function PublicLayout({
   children,
@@ -9,7 +10,9 @@ export default function PublicLayout({
   return (
     <html lang="es">
       {/* Evita scroll horizontal en móvil */}
-      <body className="overflow-x-hidden">{children}</body>
+      <body className="overflow-x-hidden">
+        <CartProvider>{children}</CartProvider>
+      </body>
     </html>
   );
 }
